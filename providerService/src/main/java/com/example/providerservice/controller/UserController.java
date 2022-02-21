15d1c4da0832b08eea024e290fd00af5718f57d0
llgan <njganlili@outlook.com>
2022-02-21 09:38:01 +0800

    fear 解决web的依赖问题

--- providerService/src/main/java/com/example/providerservice/controller/UserController.java
+++ providerService/src/main/java/com/example/providerservice/controller/UserController.java
@@ -17,8 +17,8 @@ import java.util.List;
 public class UserController {
 
     @PostMapping("/add")
-    public void addUser(User user){
-
+    public Integer addUser(User user){
+        return 1;
     }
 
 }
