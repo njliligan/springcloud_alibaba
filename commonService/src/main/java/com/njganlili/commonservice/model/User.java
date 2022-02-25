e6af1d9efdb7bc599a730d5fec58621a01bdaa83
llgan <njganlili@outlook.com>
2022-02-25 22:49:42 +0800

    fear 尝试增加openapi

--- commonService/src/main/java/com/njganlili/commonservice/model/User.java
+++ commonService/src/main/java/com/njganlili/commonservice/model/User.java
@@ -8,8 +8,23 @@ import java.util.Date;
  * @description
  * @date 2022/2/13 1:03
  */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.Valid;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-12-02T19:53:02.467132-01:00[Atlantic/Azores]")
+//@JacksonXmlRootElement(localName = "order")
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class User implements Serializable {
 
+    @JsonProperty("userName")
+//    @JacksonXmlProperty(localName = "id")
+    @Schema(description = "Updated name of the pet")
     private String userName;
 
     private Integer userAge;
@@ -30,6 +45,7 @@ public class User implements Serializable {
 
     private String id;
 
+
     public String getUserName() {
         return userName;
     }
@@ -39,6 +55,10 @@ public class User implements Serializable {
         return this;
     }
 
+    //加在get上
+    @Schema(example = "10", description = "用户年龄")
+    //甚至还能加校验= =
+    @Valid
     public Integer getUserAge() {
         return userAge;
     }
@@ -48,6 +68,8 @@ public class User implements Serializable {
         return this;
     }
 
+    //加在get上
+    @JsonProperty("sex")
     public String getUserSex() {
         return userSex;
     }
--- consumeService/src/main/java/com/njganlili/consumeservice/controller/UserController.java
+++ consumeService/src/main/java/com/njganlili/consumeservice/controller/UserController.java
@@ -2,10 +2,25 @@ package com.njganlili.consumeservice.controller;
 
 import com.njganlili.commonservice.model.User;
 import com.njganlili.consumeservice.fegin.FeignClients;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +33,15 @@ import java.util.Optional;
  * @description
  * @date 2022/2/21 15:59
  */
+@javax.annotation.Generated(value = "org.springdoc.demo.app2.codegen.languages.SpringCodegen", date = "2019-07-11T00:09:29.839+02:00[Europe/Paris]")
+
+//@SecurityScheme(name = "petstore_auth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(implicit = @OAuthFlow(authorizationUrl = "https://petstore3.swagger.io/oauth/authorize", scopes = {
+//        @OAuthScope(name = "write:pets", description = "modify pets in your account"),
+//        @OAuthScope(name = "read:pets", description = "read your pets") })))
+
 @RestController
 @RequestMapping("/user")
+@Tag(name = "放款申请Controller",description = "放款申请Controller")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -48,14 +70,39 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/addUser")
-    public Integer addUserByFeign(){
-        User user = new User();
+
+    /**
+     * 通过feign添加用户
+     * @return
+     */
+    @Scheduled
+    @GetMapping(value = "/addUser", consumes = { "application/json", "application/xml", "application/x-www-form-urlencoded" })
+    @Operation(summary = "保存放款申请",description = "保存放款申请",method = "get")
+    @ApiResponses(
+            value ={
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Order not found", content = @Content)
+            }
+    )
+//    @RequestBody(
+//            content = @Content(schema = @Schema(implementation = User.class))
+//    )
+    @Parameters(
+            value = {
+                    @Parameter(required = false,schema = @Schema(implementation = User.class))
+            }
+    )
+    public Integer addUserByFeign(User user){
         Integer result = feignClients.addUser(user);
         logger.info(String.valueOf(result));
         return result;
     }
 
+    /**
+     * 通过restTemplate添加用户
+     * @return
+     */
     @GetMapping("/addUserByRestTemplate")
     public Integer addUserByRequestTemplate(){
 //        String url = "http://provider-service/user/add";
